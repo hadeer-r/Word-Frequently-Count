f425d286c2574a150e2ceaf2d93a7cce68f48b91
Hadeer Ramadan <hadderramadan7@gmail.com>
2024-05-07 11:36:55 +0300

    editing in manager

--- src/manager/manager.cpp
+++ src/manager/manager.cpp
@@ -1,44 +1,51 @@
-#include "Manager.h"
-#include<iostream>
+#include "manager.h"
+#include <iostream>
 using namespace std;
 
-Manager :: Manager(const string& paragraph , const string& path): words(paragraph,path){}
+Manager ::Manager(const string &paragraph, const string &path) : words(paragraph, path) {}
 
 int Manager::search(const string word) const {
-  return words.search(word)
-}
+    return words.search(word)}
 
-vector<string> Manager::rank(vector<string> vecRank) {
+vector<string> Manager::rank(vector<string> vecRank)
+{
   return words.rank(vecRank);
 }
 
-vector<pair<string, int>> Manager::sorted() const {
-  return words.sorted({});
+vector<pair<string, int>> Manager::sorted() const
+{
+  return words.sorted();
 }
 
-bool Manager::startsWith(const string& prefix) const {
+bool Manager::startsWith(const string &prefix) const
+{
   return false;
 }
-void Manager::addWord(const string& word) {
+void Manager::addWord(const string &word)
+{
   checker.addWord(word);
 }
-bool Manager::isCorrect(const string& word) {
+bool Manager::isCorrect(const string &word)
+{
   return checker.isCorrect(word);
 }
-string Manager::autoCorrect(const string& word) {
+string Manager::autoCorrect(const string &word)
+{
   return checker.autoCorrect(word);
 }
-bool Manager::autoComplete(const string& prefix) {
+bool Manager::autoComplete(const string &prefix)
+{
   return checker.autoComplete(prefix);
 }
-voidt manager ::read_file(string path){ 
-     UploadTex.read_file(path)
+void Manager ::read_file(string path)
+{
+  UploadTex.read_file(path)
 }
-void manager::read_paragraph(string paragraph){
-    UploadText::read_paragraph( paragraph)
+void Manager::read_paragraph(string paragraph)
+{
+  UploadText::read_paragraph(paragraph)
 }
- void manager::from_line_to_words(string line){
-        UploadText::from_line_to_words(line)
- }
-
-
+void Manager::from_line_to_words(string line)
+{
+  UploadText::from_line_to_words(line)
+}
--- src/manager/manager.h
+++ src/manager/manager.h
@@ -4,38 +4,36 @@
 #include <string>
 #include <vector>
 #include <utility>
-#include "Words.hpp"
+#include "../manageWords/Words.hpp"
 #include "trie.h"
 #include "Checker.h"
-#ifndef UPLOAD_TEXT_HPP
-#define UPLOAD_TEXT_HPP
 #include <unordered_map>
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Manager
 {
 public:
-  Manager(const string& paragraph = "", const string& path = "");
+  Manager(const string &paragraph = "", const string &path = "");
   int search(const string word) const;
   vector<string> rank(vector<string> vecRank);
-  vector<pair<string, int>> sorted(vector<pair<string, int>> wordSort) const;
-  bool startsWith(const string& prefix) const;
-  void addWord(const string& word);
-  bool isCorrect(const string& word);
-  string autoCorrect(const string& word);
-  bool autoComplete(const string& prefix);
-   // text
-      void read_file(string path);
-    void read_paragraph(string paragraph);
-    wordMap get_wordsMap();
-    vecRank get_wordsRank();
- void from_line_to_words(string line);
+  vector<pair<string, int>> sorted() const;
+  bool startsWith(const string &prefix) const;
+  void addWord(const string &word);
+  bool isCorrect(const string &word);
+  string autoCorrect(const string &word);
+  bool autoComplete(const string &prefix);
+  // text
+  void read_file(string path);
+  void read_paragraph(string paragraph);
+  wordMap get_wordsMap();
+  vecRank get_wordsRank();
+  void from_line_to_words(string line);
 
 private:
   Words words;
   Checker checker;
   wordMap words;
-    vecRank rank;
+  vecRank rank;
 };
 #endif // MANAGER_HPP
