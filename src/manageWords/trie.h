43c45e042b4d6a92035c277bc559d9aa85dce7ab
sohaila65 <sohailamohammed111222@gmail.com>
2024-05-07 02:14:33 -0700

    insert into trie

--- src/manageWords/trie.h
+++ src/manageWords/trie.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <unordered_map>
 #include "trienode.h"
+#include "../manageText/upload_text.hpp"
 using namespace std;
 class trie {
 private:
@@ -11,12 +12,14 @@ private:
 
 public:
     trie();
+    trie( string path);
 
     void insert(const string& word);
 
     bool search(const string& word);
 
     bool startsWith(const string& prefix);
+    void readWordsFromFileAndInsert(const string& path);
 };
 
 #endif // TRIE_H
